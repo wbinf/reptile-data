f548c0eab0f2485385e609569696645eeff38a0a
wbinf <16473905@qq.com>
2020-03-23 09:20:52 +0800

    添加数据库

--- SpringBoot-HttpClient-demo/src/mian/java/com/wbf/webMagic/test/AgencyTest.java
+++ SpringBoot-HttpClient-demo/src/mian/java/com/wbf/webMagic/test/AgencyTest.java
@@ -9,6 +9,7 @@ import us.codecraft.webmagic.proxy.Proxy;
 import us.codecraft.webmagic.proxy.SimpleProxyProvider;
 import us.codecraft.webmagic.selector.Selectable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +18,8 @@ import java.util.List;
  * @Description:
  */
 public class AgencyTest implements PageProcessor {
+
+
     @Override
     public void process(Page page) {
         List < String > all = page.getHtml().$("div.well p").all();
@@ -30,11 +33,20 @@ public class AgencyTest implements PageProcessor {
     }
 
     public static void main(String[] args) {
+
         HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+        //具体实现可以将 爬取到的ip地址 和端口号添加到proxies List中
+        List<Proxy> proxies=new ArrayList <>();
+        SimpleProxyProvider simpleProxyProvider = new SimpleProxyProvider(proxies);
+        //可以添加多个ip地址
+       // httpClientDownloader.setProxyProvider(simpleProxyProvider);
         httpClientDownloader.setProxyProvider(SimpleProxyProvider.from(new Proxy("121.33.220.158",808)));
+
         Spider.create(new AgencyTest())
                 .addUrl("https://www.ip.cn/")
+                //设置ip代理
                 .setDownloader(httpClientDownloader)
                 .run();
+
     }
 }
